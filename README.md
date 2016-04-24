5000acffd4451ad44d65c5b0b98600597a6cf045
Sean Rees <sean@erifax.org>
2016-04-24 21:36:47 +0100

    Add proof-of-concept tripist binary and README

--- README.md
+++ README.md
@@ -1,1 +1,16 @@
-# tripist
+# Tripist
+
+This is a simple proof-of-concept to create Todoist projects (e.g; to
+pack) from TripIt trips. The PoC will only scan for trips upto 45 days
+away.
+
+To use this, you'll need API keys. If I know you, just ask and I'll give
+you the ones I'm using.
+
+To use this, you'll need to modify the source and call the Authorize
+functions and then update the OAuth tokens in the source. I plan to make
+this a bit more user-friendly later.
+```
+tripit.Authorize()
+todoist.Authorize()
+```
--- main.go
+++ main.go
@@ -1,25 +1,107 @@
+// Binary tripist access your trips in TripIt and creates a Todoist project.
 package main
 
 import (
 	"fmt"
 	"github.com/mrjones/oauth"
+	"github.com/seanrees/tripist/todoist"
 	"github.com/seanrees/tripist/tripit"
+	"github.com/twinj/uuid"
+	"golang.org/x/oauth2"
+	"time"
 )
 
+func strptr(s string) *string { return &s }
+
 func main() {
-  // TODO(seanrees): wire in some functionality ot call tripit.Authorize and
-  // store results.
-	// TODO(seanrees): pull in a configuration file.
+	// Authorization bits.
+	//tripit.Authorize()
+	//token := todoist.Authorize()
+
+	trips := listTrips()
+	for _, t := range trips {
+		//fmt.Printf("%#v\n", t)
+		start, err := time.Parse(time.RFC3339, t.StartDate+"T00:00:00Z")
+		if err != nil {
+			fmt.Printf("Could not parse %s: %v", t.StartDate, err)
+		}
+
+		window := time.Now().AddDate(0, 0, 45)
+		if start.After(window) {
+			continue
+		}
+
+		fmt.Printf("Trip within window: %s\n", t.DisplayName)
+
+		createProject(t.DisplayName)
+	}
+}
+
+func listTrips() []tripit.Trip {
 	at := &oauth.AccessToken{
+        // Fill this in from tripit.Authorize() results.
 		Token:  "",
 		Secret: "",
 	}
-
 	api := tripit.NewTripitV1(at)
 	trips, err := api.List(&tripit.ListParameters{Traveler: "true"})
 	if err != nil {
-		fmt.Printf("error: %v", err)
+		fmt.Printf("Could not list trips: %v", err)
 	}
 
-	fmt.Printf("%v\n", trips)
+	return trips
+}
+
+func createProject(tripName string) {
+    // Fill this in from todoist.Authorize().
+	t := &oauth2.Token{AccessToken: ""}
+	s := todoist.NewSyncV6API(t)
+
+	name := "Trip: " + tripName
+	items := []string{
+		"Pack clothes", // TODO: add how many days, wx suggestions?
+		"Pack electronics (Chromecast, chargers)",
+		"Pack toiletries",
+		"Clean litter box",
+		"Feed cats",
+		"Set heater appropriately",
+		"Pack essential documents",
+	}
+
+	// Check for pre-existing projects.
+	resp, err := s.Read([]string{todoist.Projects}, 0)
+	if err != nil {
+		fmt.Printf("Could not read Todoist projects: %v", err)
+	}
+	for _, p := range resp.Projects {
+		if *p.Name == name {
+			fmt.Printf("Project already exists, no work needed.\n")
+			return
+		}
+	}
+
+	fmt.Printf("Creating project: %q\n", name)
+
+	// Create a new project.
+	tempId := uuid.NewV4().String()
+	commands := todoist.Commands{todoist.WriteItem{
+		Type:   strptr(todoist.ProjectAdd),
+		TempId: strptr(tempId),
+		UUID:   strptr(uuid.NewV4().String()),
+		Args:   todoist.Project{Name: strptr(name)}}}
+
+	for _, i := range items {
+		commands = append(commands, todoist.WriteItem{
+			Type:   strptr(todoist.ItemAdd),
+			TempId: strptr(uuid.NewV4().String()),
+			UUID:   strptr(uuid.NewV4().String()),
+			Args: todoist.Item{
+				Content:   strptr(i),
+				ProjectId: strptr(tempId)}})
+	}
+
+	_, err = s.Write(commands)
+	if err != nil {
+		fmt.Printf("Could not create project: %v\n", err)
+	}
 }
