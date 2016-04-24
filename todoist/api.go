c71c817ea5fc0da2549b5942a22b4889be148597
Sean Rees <sean@erifax.org>
2016-04-24 19:58:03 +0100

    Refine Todoist API integration, support Write()
    This now supports Write() and can properly handle unset fields on requests
    and responses (with nil). This required the addition of stringify() for
    debugging messages, because fmt doesn't dereference pointers for us.

--- todoist/api.go
+++ todoist/api.go
@@ -2,6 +2,7 @@ package todoist
 
 import (
 	"encoding/json"
+	"fmt"
 	"golang.org/x/oauth2"
 	"io/ioutil"
 	"net/url"
@@ -19,34 +20,63 @@ func NewSyncV6API(t *oauth2.Token) *SyncV6API {
 	return &SyncV6API{token: t}
 }
 
-func (s *SyncV6API) makeRequest(url string, obj interface{}) error {
+func (s *SyncV6API) makeRequest(path string, data url.Values, obj interface{}) error {
 	c := buildConfig().Client(oauth2.NoContext, s.token)
-	resp, err := c.Get(url)
+	resp, err := c.PostForm(path, data)
+
 	if err != nil {
 		return err
 	}
 	defer resp.Body.Close()
 
-	data, err := ioutil.ReadAll(resp.Body)
+	j, err := ioutil.ReadAll(resp.Body)
 	if err != nil {
 		return err
 	}
 
-	json.Unmarshal(data, &obj)
+	if err := json.Unmarshal(j, &obj); err != nil {
+		return err
+	}
+
 	return nil
 }
 
-func (s *SyncV6API) ReadItems() ([]Item, error) {
-
+// Reads specific types and returns a ReadResponse. Possible types are in constants:
+// Projects, Items.
+//
+// sequenceNumber should be zero or a ReadResponse.SequenceNumber if you desire an
+// incremental read.
+func (s *SyncV6API) Read(types []string, sequenceNumber int) (ReadResponse, error) {
+	resp := ReadResponse{}
 	params := url.Values{}
 	params.Add("token", s.token.AccessToken)
-	params.Add("seq_no", "0")
-	params.Add("resource_types", "[\"items\"]")
+	params.Add("seq_no", fmt.Sprintf("%d", sequenceNumber))
 
-	sr := SyncResponse{}
-	err := s.makeRequest(ApiPath+"?"+params.Encode(), &sr)
+	t, err := json.Marshal(types)
 	if err != nil {
-		return nil, err
+		return resp, nil
 	}
-	return sr.Items, nil
+	params.Add("resource_types", string(t))
+
+	if err := s.makeRequest(ApiPath, params, &resp); err != nil {
+		return resp, err
+	}
+	return resp, nil
+}
+
+func (s *SyncV6API) Write(c Commands) (WriteResponse, error) {
+	resp := WriteResponse{}
+	params := url.Values{}
+	params.Add("token", s.token.AccessToken)
+
+	cmds, err := json.Marshal(c)
+	if err != nil {
+		return resp, nil
+	}
+	params.Add("commands", string(cmds))
+
+	if err := s.makeRequest(ApiPath, params, &resp); err != nil {
+		return resp, err
+	}
+	return resp, nil
 }
--- todoist/types.go
+++ todoist/types.go
@@ -1,34 +1,150 @@
 package todoist
 
+import (
+	"fmt"
+	"reflect"
+)
+
 const (
 	True  = 1
 	False = 0
+
+	// Types that can be read.
+	Items    = "items"
+	Projects = "projects"
+
+	// Types that can be written.
+	ItemAdd    = "item_add"
+	ProjectAdd = "project_add"
 )
 
-type SyncResponse struct {
-	Items []Item
+// This is like the %+v verb in fmt, but dereferences pointers.
+func stringify(obj interface{}) string {
+	t := reflect.TypeOf(obj)
+	v := reflect.ValueOf(obj)
+
+	if t.Kind() != reflect.Struct || !v.IsValid() {
+		return "error"
+	}
+
+	result := "{"
+	for i := 0; i < t.NumField(); i++ {
+		ft := t.FieldByIndex([]int{i})
+		fv := v.FieldByIndex([]int{i})
+
+		name := ft.Name
+		value := "<invalid>"
+
+		// Deference the pointer if appropriate.
+		if fv.Kind() == reflect.Ptr && !fv.IsNil() {
+			fv = fv.Elem()
+		}
+		switch {
+		case fv.Kind() == reflect.Ptr && fv.IsNil():
+			value = "<nil>"
+		case fv.Kind() == reflect.String:
+			value = fmt.Sprintf("%q", fv.Interface())
+		default:
+			value = fmt.Sprintf("%v", fv.Interface())
+		}
+
+		result += fmt.Sprintf("%s:%s ", name, value)
+	}
+	result += "}"
+
+	return result
+}
+
+type ReadResponse struct {
+	SequenceNumber *int `json:"seq_no"`
+	UserId         *int
+	Items          []Item
+	Projects       []Project
+}
+
+func (i ReadResponse) String() string {
+	return stringify(i)
+}
+
+type WriteItem struct {
+	// One of: WriteItem, WriteProject.
+	Type   *string     `json:"type"`
+	TempId *string     `json:"temp_id"`
+	UUID   *string     `json:"uuid"`
+	Args   interface{} `json:"args"`
+}
+
+func (i WriteItem) String() string {
+	return stringify(i)
+}
+
+type Commands []WriteItem
+
+type WriteResponse struct {
+	SequenceNumber *int `json:"seq_no"`
+	TempIdMapping  map[string]int
+
+	// It's one of *string ("ok") or a map[*string]*string for errors. Wtf Todoist?
+	SyncStatus interface{}
+}
+
+func (i WriteResponse) String() string {
+	return stringify(i)
 }
 
 type Item struct {
-	Id             int64   `json:"id"`
-	UserId         int64   `json:"user_id"`
-	ProjectId      int64   `json:"project_id"`
-	Content        string  `json:"content"`
-	DateString     string  `json:"date_string"`
-	DateLang       string  `json:"date_lang"`
-	DueDateUTC     string  `json:"due_date_utc"`
-	Priority       int64   `json:"priority"`
-	Indent         int64   `json:"indent"`
-	ItemOrder      int64   `json:"item_order"`
-	DayOrder       int64   `json:"day_order"`
-	Collapsed      int64   `json:"collapsed"`
-	Labels         []int64 `json:"labels"`
-	AssignedByUid  int64   `json:"assigned_by_uid"`
-	ResponsibleUid int64   `json:"responsible_uid"`
-	Checked        int64   `json:"checked"`
-	InHistory      int64   `json:"in_history"`
-	IsDeleted      int64   `json:"is_deleted"`
-	IsArchived     int64   `json:"is_archived"`
-	SyncId         int64   `json:"sync_id"`
-	DateAdded      string  `json:"date_added"`
+	Id     *int `json:"id"`
+	UserId *int `json:"user_id"`
+	// This field is normally an Integer, but in a Write, can be a *string-ified UUID.
+	ProjectId      interface{} `json:"project_id"`
+	Content        *string     `json:"content"`
+	DateString     *string     `json:"date_*string"`
+	DateLang       *string     `json:"date_lang"`
+	DueDateUTC     *string     `json:"due_date_utc"`
+	Priority       *int        `json:"priority"`
+	Indent         *int        `json:"indent"`
+	ItemOrder      *int        `json:"item_order"`
+	DayOrder       *int        `json:"day_order"`
+	Collapsed      *int        `json:"collapsed"`
+	Labels         []int       `json:"labels"`
+	AssignedByUid  *int        `json:"assigned_by_uid"`
+	ResponsibleUid *int        `json:"responsible_uid"`
+	Checked        *int        `json:"checked"`
+	InHistory      *int        `json:"in_history"`
+	IsDeleted      *int        `json:"is_deleted"`
+	IsArchived     *int        `json:"is_archived"`
+	SyncId         *int        `json:"sync_id"`
+	DateAdded      *string     `json:"date_added"`
+}
+
+func (i Item) String() string {
+	return stringify(i)
+}
+func (i Item) ProjectIdInt() int {
+	v, ok := i.ProjectId.(int)
+	if !ok {
+		return -1
+	}
+	return v
+}
+
+type Project struct {
+	Id                *int    `json:"id"`
+	UserId            *int    `json:"user_id"`
+	Name              *string `json:"name"`
+	Color             *int    `json:"color"`
+	Indent            *int    `json:"indent"`
+	ItemOrder         *int    `json:"item_order"`
+	Collapsed         *int    `json:"collapsed"`
+	Shared            *bool   `json:"shared,*string"`
+	IsDeleted         *int    `json:"is_deleted"`
+	IsArchived        *int    `json:"is_archived"`
+	ArchivedDate      *string `json:"archived_date"`
+	ArchivedTimestamp *int    `json:archived_timestamp"`
+	InboxProject      *bool   `json:"inbox_project,*string"`
+	TeamInbox         *bool   `json:"team_inbox,*string"`
+}
+
+func (i Project) String() string {
+	return stringify(i)
 }
